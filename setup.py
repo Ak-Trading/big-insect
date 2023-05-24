d96fa4dc7b41fc16ad5b9f464c0c1151baba135a
KyRillos BoshRa <kyrillos@aktrading.io>
2023-05-24 13:21:44 +0300

    delete old image on setup

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+from docker import DockerClient
 import subprocess
+
+
+client = DockerClient()
+
+images = client.images.list()
+
+for image in images:
+    if image.attrs["RepoTags"][0].split(":")[0] == "webhook":
+        image.remove()
 
 subprocess.run(["docker", "build", "-t", "webhook", "."])
 
